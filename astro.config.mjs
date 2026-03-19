a4e2b4086222f84ca393c7601dcfb52119bb48d1
Your Name <you@example.com>
2026-03-19 20:08:35 +0100

    configuracion para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: vercel(),
     vite: {
         plugins: [ tailwindcss()]
     },
