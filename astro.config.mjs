896da60d15a0869b19310dcf2f6aad977a2be3e1
Your Name <you@example.com>
2026-03-20 12:19:24 +0100

    modificado el tamaño del tituli

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-    
+    //
     adapter: vercel(),
     vite: {
         plugins: [ tailwindcss()]
